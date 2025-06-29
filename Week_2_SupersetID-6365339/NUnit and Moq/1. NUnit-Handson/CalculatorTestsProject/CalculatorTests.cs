296ef23a5432ca58042bc2fdbc991dd0978eaa1a
Kushagra Prakash Singhal <kushagraa756@gmail.com>
2025-06-29 20:44:21 +0530

    Adding The Week 2 Assignment

---
+++ Week_2_SupersetID-6365339/NUnit and Moq/1. NUnit-Handson/CalculatorTestsProject/CalculatorTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using CalcLibrary; 
+
+namespace CalculatorTestsProject
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        private SimpleCalculator calc;
+
+        [SetUp]
+        public void Setup()
+        {
+            
+            calc = new SimpleCalculator();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            
+            calc = null;
+        }
+
+        [Test]
+        [TestCase(10, 20, 30)]
+        [TestCase(-5, 5, 0)]
+        [TestCase(0, 0, 0)]
+        public void Addition_WhenCalled_ReturnsCorrectSum(double a, double b, double expected)
+        {
+            double result = calc.Addition(a, b);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test, Ignore("Demo purpose: Test is ignored.")]
+        public void ThisTestWillBeIgnored()
+        {
+            Assert.Fail("Should not run.");
+        }
+    }
+}
---
+++ Week_2_SupersetID-6365339/NUnit and Moq/1. NUnit-Handson/CalculatorTestsProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalculatorTestsProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalculatorTestsProject")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("095f4e5d-95a0-4729-a0bb-58f10d316f06")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
